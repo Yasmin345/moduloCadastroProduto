3fb8e01d66271790c83fdd0d47c6b2e31ad0b33d
Yasmin345 <Yasminmonteiros29@gmail.com>
2024-11-11 22:37:58 -0300

    feat- adicionando botão de cancelar

--- javascript/popup.js
+++ javascript/popup.js
@@ -3,6 +3,7 @@ const botaoCad = document.getElementById('bot');
 const botaoCat = document.getElementById('bot2');
 const janelaPopup = document.getElementById('janela_popup');
 const janelaCat = document.getElementById('janela');
+const botaoCancelar = document.getElementById('cancelar'); 
 
 
 botaoCad.addEventListener('click', () => {
@@ -13,11 +14,17 @@ botaoCat.addEventListener('click', () => {
     janelaCat.style.display = 'block';
 });
 
+
 window.addEventListener('click', (event) => {
     if (event.target === janelaPopup) {
         janelaPopup.style.display = 'none';
-    } 
+    }
     if (event.target === janelaCat) {
         janelaCat.style.display = 'none';
     }
+});
+
+
+botaoCancelar.addEventListener('click', () => {
+    janelaPopup.style.display = 'none'; 
 });
